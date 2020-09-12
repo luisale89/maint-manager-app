323e7771de84e053ee8070a07a75b68645d8eaf1
luis.asus.ubuntu <luis.lucena89@gmail.com>
2020-09-12 19:30:18 -0300

    small changes

--- app/utils/rules.py
+++ app/utils/rules.py
@@ -6,7 +6,7 @@ from webassets.filter import get_filter
 libsass = get_filter( #se crea un filtro a partir de libsass, con todas las configuraciones necesarias
     'libsass',
     style='nested', #parametro define el estilo de salida del bundle. En prod. debe ser 'compressed'
-    includes=['./src/static/scss'], #lista con las rutas que contienen los .scss importados en el index.scss
+    includes=['./app/static/scss'], #lista con las rutas que contienen los .scss importados en el index.scss
     as_output=True
 )
 
