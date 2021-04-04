ab905383b6b7df3c468e0d105609a1b3ca0ab25b
luis-alejandro <luis.lucena89@gmail.com>
2021-04-04 09:25:04 -0400

    profile model added

--- app/blueprints/admin/admin.py
+++ app/blueprints/admin/admin.py
@@ -4,10 +4,11 @@ from app.extensions import (
 )
 from flask_admin.contrib.sqla import ModelView
 from app.models.auth import (
-    User, TokenBlacklist
+    User, TokenBlacklist, Profile
 )
 
 admin_bp = Blueprint('admin_bp', __name__)
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(TokenBlacklist, db.session))
+admin.add_view(ModelView(Profile, db.session))
--- app/models/auth.py
+++ app/models/auth.py
@@ -3,18 +3,20 @@ from app.extensions import db
 from datetime import datetime
 
 from werkzeug.security import generate_password_hash
-# from sqlalchemy.dialects.postgresql import JSON #only for postgresql
+from sqlalchemy.dialects.postgresql import JSON
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(60), unique=True)
+    public_id = db.Column(db.String(60), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256))
+    password_hash = db.Column(db.String(256), nullable=False)
     fname = db.Column(db.String(60), nullable=False)
     lname = db.Column(db.String(60), nullable=False)
-    profile_img = db.Column(db.String(120))
     user_since = db.Column(db.DateTime, default=datetime.utcnow)
+    #relationships
+    profile = db.relationship('Profile', back_populates='user', uselist=False, lazy=True) #1-1 with profile
 
     def __repr__(self):
         return '<User %r>' % self.public_id
@@ -24,8 +26,8 @@ class User(db.Model):
             "public_id" : self.public_id,
             "fname" : self.fname,
             "lname" : self.lname,
-            "profile_img": self.profile_img if self.profile_img is not None else "https://server.com/default.png",
-            "user_since": self.user_since
+            "user_since": self.user_since,
+            "profile": self.profile.serialize()
         }
 
     @property
@@ -37,7 +39,29 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password, method='sha256')
 
 
+class Profile(db.Model):
+    __tablename__ = 'profile'
+    id = db.Column(db.Integer, primary_key=True)
+    profile_img = db.Column(db.String(120))
+    home_address = db.Column(JSON)
+    personal_phone = db.Column(db.String(30))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #relationships
+    user = db.relationship('User', back_populates='profile', uselist=False, lazy=True) #1-1 with user
+
+    def __repr__(self):
+        return '<Profile %r>' % self.id
+
+    def serialize(self):
+        return {
+            "profile_img": self.profile_img if self.profile_img is not None else "https://server.com/default.png",
+            'home_address': self.home_address,
+            'personal_phone': self.personal_phone
+        }
+
+
 class TokenBlacklist(db.Model):
+    __tablename__ = 'tokenblacklist'
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(36), nullable=False)
     token_type = db.Column(db.String(10), nullable=False)
