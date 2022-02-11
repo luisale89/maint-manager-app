f8ce33df0cf340820a2392cfcf55375e7c51f282
Luis Lucena <luis.lucena89@gmail.com>
2022-02-11 08:38:57 -0300

    new lines

--- app/utils/token_factory.py
+++ app/utils/token_factory.py
@@ -1,8 +1,10 @@
 import os
+import datetime
 from itsdangerous import (
     URLSafeTimedSerializer, BadData
 )
 from random import randint
+from flask_jwt_extended import create_access_token
 
 serializer = URLSafeTimedSerializer(os.environ['SECRET_KEY'])
 
@@ -59,15 +61,12 @@ def validate_url_token(token:str, salt:str, identifier:str=None, age:int=600) ->
     return {"valid": True, "id": identity}
 
 
-def create_validation_code(identity:str= None, salt:str= None) -> dict:
-    
-    random_code = randint(1000000, 999999)
+def create_validation_code(user_email:str= None, salt:str= None) -> dict:
 
-    if identity is None or salt is None:
+    if user_email is None or salt is None:
         raise ValueError("one or all function's parameter is missing")
 
-    token = serializer.dumps(identity, salt=salt)
     return {
-        "validation_token": token,
-        "random_code": random_code
+        "validation_token": create_access_token(identity=user_email, expires_delta=datetime.timedelta(days=2)), #token valid for two days
+        "random_code": randint(100000, 999999)
     }
