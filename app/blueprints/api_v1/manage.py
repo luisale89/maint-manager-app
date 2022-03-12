37655a439d41290a92af291cd11b46bbb0188ce7
Luis Lucena <luis.lucena89@gmail.com>
2022-03-12 14:37:46 -0300

    super-user required added

--- app/blueprints/api_v1/manage.py
+++ app/blueprints/api_v1/manage.py
@@ -5,21 +5,15 @@ from flask import (
 from app.models.main import Role, Plan
 from app.utils.exceptions import APIException
 from app.utils.helpers import JSONResponse
-from app.utils.decorators import (json_required, user_required)
-from flask_jwt_extended import get_jwt_identity
+from app.utils.decorators import (json_required, super_user_required)
 
 manage_bp = Blueprint('manage_bp', __name__)
 
 
 @manage_bp.route('/set-globals', methods=['GET']) #!debug
 @json_required()
-@user_required()
+@super_user_required()
 def set_app_globals():
-
-    user = get_jwt_identity()
-    if user != 'luis.lucena89@gmail.com': #* super usuario de la app, se debe ajustar variable de entorno
-
-        raise APIException(message="unautorized user", status_code=401)
 
     Role.add_default_roles()
     Plan.add_default_plans()
